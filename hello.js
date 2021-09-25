10c18d3db36eaf6ec3689d54928d68d4973fcd05
Jamie Li <lijamie19828@gmail.com>
2021-09-25 20:25:03 +0000

    conditionals.js and hello.js were updated

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 //code example 1
-/*const sayHello  = function () {
+/*
+const sayHello  = function () {
   console.log("Hello, world");
 }
 
@@ -12,3 +13,19 @@ const sayHello = function (name) {
 sayHello("Kevin");
 sayHello("Jamie");
 sayHello("James");
+
+/* return vs console.log
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+//returns output to the console
+*/
+
+
+/*const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+//does not return an output to console
+*/
