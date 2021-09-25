93409cc63e910af80eb364921b53ec91559ec810
Jamie Li <lijamie19828@gmail.com>
2021-09-25 20:14:30 +0000

    added Loopylighthouse challenge and hello.js function examples

---
+++ hello.js
@@ -0,0 +1,14 @@
+//code example 1
+/*const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();*/
+
+//code example 2
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("Kevin");
+sayHello("Jamie");
+sayHello("James");
