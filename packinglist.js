23f87ae28d9d08d58c458fd02b25b68934fbfd16
Jamie Li <lijamie19828@gmail.com>
2021-09-27 20:19:56 +0000

    updated with while loop as well as for loop

--- packinglist.js
+++ packinglist.js
@@ -1,6 +1,16 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+/* for loop - i declared, condition and how to move along is included in the ( ) of the for loop
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+*/
+
+/* while loop - i, condition and how to move on to the next step is declared individually*/
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
