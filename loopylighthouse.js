7cd51278ef668e17e92dcba52fb0e1cfb3e0c223
Jamie Li <lijamie19828@gmail.com>
2021-09-29 19:06:21 +0000

    commented out initial loopylighthouse, added loopylighthouse 2 challenge

--- loopylighthouse.js
+++ loopylighthouse.js
@@ -1,3 +1,4 @@
+/* loopylighthouse 1
 for (var x = 100; x <= 200; x++) {
   if (x % 3 === 0 && x % 4 === 0) {
     console.log('LoopyLighthouse')
@@ -9,3 +10,22 @@ for (var x = 100; x <= 200; x++) {
     console.log(x)
   }
 }
+*/
+
+//loopylighthouse 2
+function loopyLighthouse(range, multiples, words) {
+  let y = multiples[0] * multiples[1]
+  for (var i = range[0]; i <= range[1]; i++) {
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    } else if (i % multiples[0] === 0) {
+      console.log(words[0]);
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
